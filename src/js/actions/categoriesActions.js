317d078aaa568f01c8b418c2f42f51dbf655c61f
Rustem Islamuratov <rustem.islamuratov@gmail.com>
2017-01-22 00:01:10 +0500

    0.1.4 ES6 style to categories actions

--- src/js/actions/categoriesActions.js
+++ src/js/actions/categoriesActions.js
@@ -7,49 +7,41 @@ import axios from "axios";
  * Fetches all categories
  * @returns {{type: string, payload: *}}
  */
-export function fetchCategories() {
-    return {
-        type: "FETCH_CATEGORIES",
-        payload: axios.get("http://localhost:4000/categories")
-    }
-}
+export const fetchCategories = () => ({
+    type: "FETCH_CATEGORIES",
+    payload: axios.get("http://localhost:4000/categories")
+});
 
 /**
  * Adds category
  * @param category - {name, icon} - category to add
  * @returns {{type: string, payload: *}}
  */
-export function addCategory(category) {
-    console.log("Adding category:", category);
-    return {
-        type: "ADD_CATEGORY",
-        payload: axios.post("http://localhost:4000/categories", category)
-    }
-}
+export const addCategory = (category) => ({
+    type: "ADD_CATEGORY",
+    payload: axios.post("http://localhost:4000/categories", category)
+});
 
 /**
  * Updates category
  * @param category - {id, name, icon} - category to update
  * @returns {{type: string, payload: *}}
  */
-export function updateCategory(category) {
-    return {
-        type: "UPDATE_CATEGORY",
-        payload: axios.put("http://localhost:4000/categories/" + category.id, category)
-    }
-}
+export const updateCategory = (category) => ({
+    type: "UPDATE_CATEGORY",
+    payload: axios.put("http://localhost:4000/categories/" + category.id, category)
+});
 
 /**
  * Deletes category
  * @param id - ID of deleting category
  * @returns {{type: string, payload: *}}
  */
-export function deleteCategory(id) {
-    console.log("Deleting category:", id);
+export const deleteCategory = (id) => {
     // TODO: find out how to properly pass id to reducer using axios in payload
     axios.delete("http://localhost:4000/categories/" + id);
     return {
         type: "DELETE_CATEGORY",
         payload: id
     }
-}
+};
